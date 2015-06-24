3e81f830779e11d0d6f473a00996fd3bdfc39e93
Szia <sziaru@users.noreply.github.com>
2015-06-25 00:31:35 +0600

    Update net.py

--- net.py
+++ net.py
@@ -6,3 +6,8 @@ def sign_in_matrix(server, user_id, password):
     client = Client.MatrixClient(server)
     access_token = client.login_with_password(user_id, password)
     return client, access_token
+
+def update_room_details(room):
+    room.update_room_name()
+    room.update_room_topic()
+    room.update_aliases()
